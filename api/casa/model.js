ed516d281385f4982d7491d38aa442eb57a311ef
Fabian Escarate <fabian.escarate@fullprogrammers.cl>
2021-03-21 01:41:29 -0300

    model casa

---
+++ api/casa/model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const ModelCondominio = require('../condominio/model.js');
+
+const modelName = 'casa';
+
+const ModelCasa = new mongoose.model(
+    modelName,
+    new mongoose.Schema({
+        name: String,
+        number:{
+            type: Number,
+            // unique: true,
+            index: true
+        },
+        condominio: ModelCondominio.prototype.schema,
+        address: {
+            type: String,
+            get: (v) => `Ubicación dentro del condominio ${v}`
+        },
+        location: {
+            lat: String,
+            lon: String
+        }
+    }, {
+        collection: modelName,
+        autoCreate: true
+    })
+)
+
+module.exports = ModelCasa
