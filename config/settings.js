fd531556ce9fc5cd56ca3dec4d8c6eed92011645
Fabian Escarate <fabian.escarate@fullprogrammers.cl>
2021-05-01 20:42:05 -0400

    feat: integracion de morgan para logs

--- config/settings.js
+++ config/settings.js
@@ -1,34 +1,28 @@
-require('dotenv').config()
-const path = require('path')
-const hbs = require('hbs')
-const hbsUtils = require('hbs-utils')(hbs)
+require("dotenv").config();
+const path = require("path");
+const hbs = require("hbs");
+const hbsUtils = require("hbs-utils")(hbs);
 
-__dirname = path.resolve(path.join(__dirname, '../'))
+__dirname = path.resolve(path.join(__dirname, "../"));
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-process.env.HOST = process.env.HOST || '0.0.0.0'
-process.env.PORT = process.env.PORT || 5000
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+process.env.HOST = process.env.HOST || "0.0.0.0";
+process.env.PORT = process.env.PORT || 5000;
 
 const loadConfig = (app) => {
+  app.use(require("express").json()); // for parsing application/json
 
-    app.use(require('express').json()) // for parsing application/json
+  // TEMPLATES
+  app.set("view engine", "html");
+  app.engine("html", hbs.__express);
+  app.set("views", "templates");
+  hbsUtils.registerWatchedPartials(__dirname + "/templates");
 
-    // TEMPLATES
-    app.set('view engine', 'html')
-    app.engine('html', hbs.__express)
-    app.set('views', 'templates')
-    hbsUtils.registerWatchedPartials(__dirname + '/templates')
-    
-    // hbs.registerPartials(__dirname + '/templates/partials',
-    //     (err) => {
-    //         console.error('error al cargar parciales', err, path.resolve(path.join(__dirname, '/templates')))
-    //     }
-    // )
-    // console.log('partials', hbs.handlebars.partials)
+  // LOGS
+  app.use(require('morgan')('common'))
 
-    // ASSETS
-    app.use(require('express').static(path.join(__dirname, '/public')))
-}
+  // ASSETS
+  app.use(require("express").static(path.join(__dirname, "/public")));
+};
 
-
-module.exports = loadConfig
+module.exports = loadConfig;
